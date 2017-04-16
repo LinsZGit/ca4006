8bb2fcfbb8d1af6cc991cfffe3089fbbc4674270
LinsZGit <verayumao@gmail.com>
2017-04-16 16:20:05 +0100

    Update PropertyResource.java

--- Desktop/Java/com.vogella.jersey.first/src/com/vogella/jersey/first/PropertyResource.java
+++ Desktop/Java/com.vogella.jersey.first/src/com/vogella/jersey/first/PropertyResource.java
@@ -16,14 +16,14 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
  
-@Path("/properties")/*cust’r service’s relative root URI*/  
+@Path("/propertys")/*cust’r service’s relative root URI*/  
 
-public class PropertyResource {
-	
+public class PropertysResource {
+	private Date today = new Date();
 	private Map<Integer, Property> propertyDB = new  ConcurrentHashMap<Integer, Property>();   
 	private AtomicInteger idCounter = new AtomicInteger();    
 	
-	public PropertyResource() {  }   
+	public PropertysResource() {  }   
 	@POST   
 	@Consumes("application/xml")  
 	
@@ -32,7 +32,7 @@ public class PropertyResource {
 		property1.setId(idCounter.incrementAndGet());    
 		propertyDB.put(property1.getId(), property1);      
 		System.out.println("Created property " +  property1.getId());      
-		return Response.created(URI.create("/properties/" +  property1.getId())).build(); 
+		return Response.created(URI.create("/propertys/" +  property1.getId())).build(); 
 	}
 	
 	@GET   
@@ -49,8 +49,50 @@ public class PropertyResource {
 			}      
 		};
 	}
+	@GET
+	@Path("/min={min}&&max={max}")
+	@Produces("application/xml") 
+	public StreamingOutput findProperty(@PathParam("min") int min,@PathParam("max") int max){
+		if (propertyDB.isEmpty()) {         
+			throw new WebApplicationException(Response.Status.NOT_FOUND);      
+		}
+		List<Property> result = new ArrayList<>();
+		Set<Integer> id = propertyDB.keySet();
+		for(Integer i: id){
+			Property prob = propertyDB.get(i);
+			int price = Integer.parseInt(prob.getPrice());
+			String[] start = (prob.getStart()).split("/");
+			Date startDate = new Date(Integer.parseInt(start[2]),Integer.parseInt(start[1]),Integer.parseInt(start[0]));
+			String[] end = (prob.getEnd()).split("/");
+			Date endDate = new Date(Integer.parseInt(end[2]),Integer.parseInt(end[1]),Integer.parseInt(end[0]));
+			if(price >= min && price <= max){
+				result.add(prob);
+			}
+			
+		}
+		
+		return new StreamingOutput() {         
+			public void write(OutputStream outputStream) throws IOException, WebApplicationException {            
+				outputPropertys(outputStream, result);         
+			}      
+		};
+	}
 	
-
+	/*
+	@GET   
+	@Path("min = {min_price}")   
+	@Produces("application/xml")   
+	public StreamingOutput getRange(@PathParam("min_price") int min) {      
+		final Property prob = propertyDB.get(id);      
+		if (prob == null) {         
+			throw new WebApplicationException(Response.Status.NOT_FOUND);      
+		}      
+		return new StreamingOutput() {         
+			public void write(OutputStream outputStream) throws IOException, WebApplicationException {            
+				outputProperty(outputStream, prob);         
+			}      
+		};
+	}*/
 	
 	@PUT 
 	@Path("{id}")
@@ -59,18 +101,37 @@ public class PropertyResource {
 		Property update = readProperty(is);      
 		Property property1 = propertyDB.get(id);      
 		if (property1 == null) throw new WebApplicationException(Response.Status.NOT_FOUND); 
-	 
+		property1.setType(update.getType()); 
 		property1.setDistrict(update.getDistrict());      
 		property1.setPrice(update.getPrice());      
 	}
-	 
+	
+	
+	protected void outputPropertys(OutputStream os, List<Property> propertys) throws IOException {  
+		PrintStream writer = new PrintStream(os);
+		writer.println("<property_list>");  
+		for(Property prop: propertys){
+			writer.println("   <property id=\"" + prop.getId() + "\">");  
+			writer.println("      <type>" + prop.getType() + "</type>");  
+			writer.println("      <district>" + prop.getDistrict() + "</district>");      
+			writer.println("      <bedroom>" + prop.getBedroom() + "</bedroom>"); 
+			writer.println("      <price>" + prop.getPrice() + "</price>");    
+			writer.println("      <startTime>" + prop.getStart() + "</startTime>"); 
+			writer.println("      <endTime>" + prop.getEnd() + "</endTime>"); 
+			writer.println("   </property>"); 
+		}
+		writer.println("</property_list>"); 
+	}
 	 
 	protected void outputProperty(OutputStream os, Property prop) throws IOException {      
 		PrintStream writer = new PrintStream(os);      
-		writer.println("<property id=\"" + prop.getId() + "\">");      
+		writer.println("<property id=\"" + prop.getId() + "\">");  
+		writer.println("   <type>" + prop.getType() + "</type>");  
 		writer.println("   <district>" + prop.getDistrict() + "</district>");      
 		writer.println("   <bedroom>" + prop.getBedroom() + "</bedroom>"); 
-		writer.println("   <price>" + prop.getPrice() + "</price>");          
+		writer.println("   <price>" + prop.getPrice() + "</price>");    
+		writer.println("   <startTime>" + prop.getStart() + "</startTime>"); 
+		writer.println("   <endTime>" + prop.getEnd() + "</endTime>"); 
 		writer.println("</property>");   
 	}
 	
@@ -85,16 +146,25 @@ public class PropertyResource {
 				prop.setId(Integer.valueOf(root.getAttribute("id")));         
 			NodeList nodes = root.getChildNodes();         
 			for (int i = 0; i < nodes.getLength(); i++) {            
-				Element element = (Element) nodes.item(i);            
-				if (element.getTagName().equals("district")) {               
+				Element element = (Element) nodes.item(i); 
+				if (element.getTagName().equals("type")){               
+					prop.setType(element.getTextContent());            
+				}
+				else if (element.getTagName().equals("district")) {               
 					prop.setDistrict(element.getTextContent());            
-				}   
+				}
 				else if (element.getTagName().equals("bedroom")){               
 					prop.setBedroom(element.getTextContent());            
 				} 
 				else if (element.getTagName().equals("price")){               
 					prop.setPrice(element.getTextContent());            
-				}                   
+				} 
+				else if (element.getTagName().equals("startTime")){               
+					prop.setStart(element.getTextContent());            
+				}
+				else if (element.getTagName().equals("endTime")){               
+					prop.setEnd(element.getTextContent());            
+				}
 			}         
 				
 			return prop;     

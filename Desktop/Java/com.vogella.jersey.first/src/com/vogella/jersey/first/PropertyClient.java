0ccf86352d6e858c985c20c02db2f0290f792b41
LinsZGit <verayumao@gmail.com>
2017-04-16 18:53:53 +0100

    Update PropertyClient.java

--- Desktop/Java/com.vogella.jersey.first/src/com/vogella/jersey/first/PropertyClient.java
+++ Desktop/Java/com.vogella.jersey.first/src/com/vogella/jersey/first/PropertyClient.java
@@ -16,8 +16,8 @@ public class PropertyClient {
 					+ "<district>3</district>"                 
 					+ "<bedroom>2</bedroom>"                 
 					+ "<price>375000</price>"       
-					+ "<startTime>15/04/2017</startTime>" 
-					+ "<endTime>30/04/2017</endTime>" 
+					+ "<startTime>2017-01-15</startTime>" 
+					+ "<endTime>2017-01-30</endTime>" 
 					+ "</property>"; 
 
 			xml[1] = "<property>"
@@ -25,32 +25,32 @@ public class PropertyClient {
 					+ "<district>5</district>"                 
 					+ "<bedroom>3</bedroom>"                 
 					+ "<price>360000</price>"  
-					+ "<startTime>15/03/2017</startTime>" 
-					+ "<endTime>30/03/2017</endTime>" 
+					+ "<startTime>2017-03-15</startTime>" 
+					+ "<endTime>2017-03-30</endTime>" 
 					+ "</property>"; 
 			xml[2] = "<property>"
 					+ "<type>H3</type>"  
 					+ "<district>3</district>"                 
 					+ "<bedroom>3</bedroom>"                 
 					+ "<price>500000</price>"  
-					+ "<startTime>15/01/2017</startTime>" 
-					+ "<endTime>30/08/2017</endTime>" 
+					+ "<startTime>2017-01-15</startTime>" 
+					+ "<endTime>2017-08-30</endTime>" 
 					+ "</property>"; 
 			xml[3] = "<property>"
 					+ "<type>A1</type>"  
 					+ "<district>5</district>"                 
 					+ "<bedroom>2</bedroom>"                 
 					+ "<price>250000</price>"
-					+ "<startTime>30/04/2017</startTime>" 
-					+ "<endTime>30/06/2017</endTime>" 
+					+ "<startTime>2017-04-30</startTime>" 
+					+ "<endTime>2017-06-30</endTime>" 
 					+ "</property>"; 
 			xml[4] = "<property>"
 					+ "<type>A2</type>"  
 					+ "<district>7</district>"                 
 					+ "<bedroom>1</bedroom>"                 
 					+ "<price>150000</price>" 
-					+ "<startTime>15/02/2017</startTime>" 
-					+ "<endTime>30/04/2018</endTime>" 
+					+ "<startTime>2017-02-15</startTime>" 
+					+ "<endTime>2018-04-30</endTime>" 
 					+ "</property>"; 
 			for(String s: xml){
 				Response response = client.target("http://localhost:8080/com.vogella.jersey.first/services/propertys").request().post(Entity.xml(s)); 
@@ -65,24 +65,20 @@ public class PropertyClient {
 			}
 			
 			/* test GET method */         
-			/*System.out.println("*** GET Created Customer **");   
-			String location = "http://localhost:8080/com.vogella.jersey.first/services/propertys/100";
-			String customer =  client.target(location).request().get(String.class);         
-			System.out.println(customer); 
-			 */
+			 
 	        String updateCust = "<property>"
 	        		+ "<type>H5</type>"  
 					+ "<district>700000</district>"                 
 					+ "<bedroom>1</bedroom>"                 
 					+ "<price>150000</price>" 
-					+ "<startTime>15/08/2017</startTime>" 
-					+ "<endTime>30/12/2017</endTime>" 
+					+ "<startTime>2017-08-15</startTime>" 
+					+ "<endTime>2017-09-10</endTime>" 
 					+ "</property>"; 
 	        String location = "http://localhost:8080/com.vogella.jersey.first/services/propertys/3";
 	 
-	        /* test PUT method */         
+	        /* test PUT method */     
 	        Response response = client.target(location).request().put(Entity.xml(updateCust));         
-	        if (response.getStatus() != 204) throw new  RuntimeException("Failed to update");         
+	        //if (response.getStatus() != 204) throw new  RuntimeException("Failed to update");         
 	        response.close(); 
 	 
 	        //System.out.println("**** After Update ***");         
